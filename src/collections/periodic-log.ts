57d6a70f05e6bba2bcec64a1b2c18e3f372ad1e2
Brian Rodriguez <brian@brianrodri.com>
2023-09-18 05:31:51 -0400

    Comprehensive tests

--- src/collections/periodic-log.ts
+++ src/collections/periodic-log.ts
@@ -2,12 +2,34 @@ import { DateTime, Duration, Interval } from "luxon";
 
 export class PeriodicLog {
     public readonly kind = "periodic-log" as const;
+    public readonly folder: string;
     public readonly period: Duration;
     public readonly offset: Duration;
 
     constructor(cfg: PeriodicLogConfig) {
+        this.validate(cfg);
+        this.folder = cfg.folder;
         this.period = Duration.fromISO(cfg.period);
-        this.offset = Duration.fromISO(cfg.offset ?? "PT0S");
+        this.offset = cfg.offset ? Duration.fromISO(cfg.offset) : Duration.fromMillis(0);
+    }
+
+    validate(cfg: PeriodicLogConfig) {
+        if (cfg.kind !== this.kind) {
+            throw new Error(`kind must be "${this.kind}" but got value: ${JSON.stringify(cfg.kind)}`);
+        }
+        if (!cfg.folder) {
+            throw new Error(`folder is required but got value: ${JSON.stringify(cfg.folder)}`);
+        }
+        const period = Duration.fromISO(cfg.period);
+        if (!period.isValid) {
+            const error = `${period.invalidReason}: ${period.invalidExplanation}`;
+            throw new Error(`period must be valid but got error: ${JSON.stringify(error)}`);
+        }
+        const offset = cfg.offset ? Duration.fromISO(cfg.offset) : Duration.fromMillis(0);
+        if (!offset.isValid) {
+            const error = `${offset.invalidReason}: ${offset.invalidExplanation}`;
+            throw new Error(`offset must be valid but got error: ${JSON.stringify(error)}`);
+        }
     }
 
     getInterval(fileName: string): Interval {
