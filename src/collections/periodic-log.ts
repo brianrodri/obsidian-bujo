3273487f175ea80e78e0f77e63dd17632ee0c075
Brian Rodriguez <brian@brianrodri.com>
2023-09-18 03:53:32 -0400

    Add unit tests for periodic log

--- src/collections/periodic-log.ts
+++ src/collections/periodic-log.ts
@@ -2,18 +2,18 @@ import { DateTime, Duration, Interval } from "luxon";
 
 export class PeriodicLog {
     public readonly kind = "periodic-log" as const;
-    public readonly duration: Duration;
+    public readonly period: Duration;
     public readonly offset: Duration;
 
     constructor(cfg: PeriodicLogConfig) {
-        this.duration = Duration.fromISO(cfg.duration);
+        this.period = Duration.fromISO(cfg.period);
         this.offset = Duration.fromISO(cfg.offset ?? "PT0S");
     }
 
     getInterval(fileName: string): Interval {
         const fileDate = DateTime.fromISO(fileName);
         const start = fileDate.plus(this.offset);
-        const end = start.plus(this.duration);
+        const end = start.plus(this.period);
         return start.until(end);
     }
 }
@@ -21,6 +21,6 @@ export class PeriodicLog {
 export type PeriodicLogConfig = {
     readonly kind: PeriodicLog["kind"];
     readonly folder: string;
-    readonly duration: string;
+    readonly period: string;
     readonly offset?: string;
 }
--- tst/collections/periodic-log.test.ts
+++ tst/collections/periodic-log.test.ts
@@ -1,48 +1,35 @@
 import { describe, expect, it } from "vitest";
-import { PeriodicLog, PeriodicLogConfig } from "src/collections/periodic-log";
-import { DateTime, Interval } from "luxon";
+import { PeriodicLog } from "src/collections/periodic-log";
+import { Interval } from "luxon";
+
+// September 2023
+// ==============
+// |     | Mon | Tue | Wed | Thu | Fri | Sat | Sun |
+// |:---:| ---:| ---:| ---:| ---:| ---:| ---:| ---:|
+// | W35 |     |     |     |     |   1 |   2 |   3 |
+// | W36 |   4 |   5 |   6 |   7 |   8 |   9 |  10 |
+// | W37 |  11 |  12 |  13 |  14 |  15 |  16 |  17 |
+// | W38 |  18 |  19 |  20 |  21 |  22 |  23 |  24 |
+// | W39 |  25 |  26 |  27 |  28 |  29 |  30 |     |
 
 describe("PeriodicLog", () => {
-    const DAILY_LOG_CONFIG: PeriodicLogConfig = {
-        kind: "periodic-log",
-        folder: "Daily Logs",
-        duration: "P1D",
-    };
-    const WEEKLY_LOG_CONFIG: PeriodicLogConfig = {
-        kind: "periodic-log",
-        folder: "Weekly Logs",
-        duration: "P1W",
-    };
-    const SPRINT_LOG_CONFIG: PeriodicLogConfig = {
-        kind: "periodic-log",
-        folder: "Sprint Logs",
-        duration: "P2W",
-        offset: "P3D",
-    };
-
-    it("has a distinct kind", () => {
-        const log = new PeriodicLog(DAILY_LOG_CONFIG);
-        expect(log.kind).toEqual("periodic-log");
+    it("returns daily interval", () => {
+        const log = new PeriodicLog({kind: "periodic-log", folder: "Daily Logs", period: "P1D"});
+        expect(log.getInterval("2023-09-17")).toEqual(Interval.fromISO("2023-09-17/2023-09-18"));
     });
 
-    it("returns interval for day", () => {
-        const log = new PeriodicLog(DAILY_LOG_CONFIG);
-        const itvl = log.getInterval("2023-09-17");
-        expect(itvl).toEqual(Interval.fromDateTimes(DateTime.fromISO("2023-09-17"),
-                                                    DateTime.fromISO("2023-09-18")));
+    it("returns weekly interval", () => {
+        const log = new PeriodicLog({kind: "periodic-log", folder: "Weekly Logs", period: "P1W"});
+        expect(log.getInterval("2023-W37")).toEqual(Interval.fromISO("2023-09-11/2023-09-18"));
     });
 
-    it("returns interval for week", () => {
-        const log = new PeriodicLog(WEEKLY_LOG_CONFIG);
-        const itvl = log.getInterval("2023-W37");
-        expect(itvl).toEqual(Interval.fromDateTimes(DateTime.fromISO("2023-09-11"),
-                                                    DateTime.fromISO("2023-09-18")));
+    it("returns sprint interval (2 weeks long, starts on thursday)", () => {
+        const log = new PeriodicLog({kind: "periodic-log", folder: "Sprint Logs", period: "P2W", offset: "P3D"});
+        expect(log.getInterval("2023-W37")).toEqual(Interval.fromISO("2023-09-14/2023-09-28"));
     });
 
-    it("returns interval for sprint (2 weeks long, start on thursday)", () => {
-        const log = new PeriodicLog(SPRINT_LOG_CONFIG);
-        const itvl = log.getInterval("2023-W37");
-        expect(itvl).toEqual(Interval.fromDateTimes(DateTime.fromISO("2023-09-14"),
-                                                    DateTime.fromISO("2023-09-28")));
+    it("returns monthly interval", () => {
+        const log = new PeriodicLog({kind: "periodic-log", folder: "Monthly Logs", period: "P1M"});
+        expect(log.getInterval("2023-09")).toEqual(Interval.fromISO("2023-09-01/2023-10-01"));
     });
 });
