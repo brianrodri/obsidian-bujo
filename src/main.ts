e7425b4b56c9179b993a3d797286dd98b6429e4b
Brian Rodriguez <brian@brianrodri.com>
2023-09-24 07:49:48 -0400

    Keep Obsidian-dependent code in main.ts -- it can't be imported from jest

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
-export { ObsidianBujo as default } from "plugin";
+import { Plugin } from "obsidian";
+import { ObsidianBujoSettings } from "settings/settings";
+
+export const DEFAULT_SETTINGS = {} as ObsidianBujoSettings;
+
+export default class ObsidianBujo extends Plugin {
+    private settings: ObsidianBujoSettings;
+
+    override async onload() {
+        await this.loadSettings();
+    }
+
+    override async onunload() {
+        await this.saveSettings();
+    }
+
+    getSettings(): ObsidianBujoSettings {
+        return this.settings;
+    }
+
+    setSettings(settings: Partial<ObsidianBujoSettings>) {
+        this.settings = structuredClone({ ...this.settings, ...settings });
+    }
+
+    async loadSettings() {
+        this.settings = structuredClone({ ...DEFAULT_SETTINGS, ...(await this.loadData()) });
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+}
