5db54901ad778bcade8c195b4c5368db8704cbd2
Brian Rodriguez <brianrz@amazon.com>
2023-09-24 09:27:20 -0400

    Add test ensuring default collections are valid

---
+++ src/settings/__tests__/settings.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "@jest/globals";
+import { PeriodicLog } from "collections/periodic-log";
+import { DEFAULT_SETTINGS } from "settings/settings";
+
+describe("DEFAULT_SETTINGS", () => {
+    it.each(DEFAULT_SETTINGS.collections.periodic)("holds valid collections.periodic[%#] = %j", (config) => {
+        expect(() => new PeriodicLog(config)).not.toThrow();
+    });
+});
