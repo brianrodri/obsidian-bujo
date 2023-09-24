a1a2b79c875f0ce42199e17d71eeaa112608b813
Brian Rodriguez <brian@brianrodri.com>
2023-09-24 06:39:00 -0400

    Split up plugin/settings into their own file

---
+++ src/plugin.ts
@@ -0,0 +1,32 @@
+import { Plugin } from "obsidian";
+import { ObsidianBujoSettings } from "settings/settings";
+
+export const DEFAULT_SETTINGS = {} as ObsidianBujoSettings;
+
+export class ObsidianBujo extends Plugin {
+    private settings: ObsidianBujoSettings;
+
+    override async onload() {
+        await this.loadSettings();
+    }
+
+    override async onunload() {
+        await this.saveSettings();
+    }
+
+    getSettings(): ObsidianBujoSettings {
+        return this.settings;
+    }
+
+    setSettings(settings: Partial<ObsidianBujoSettings>) {
+        this.settings = structuredClone({ ...this.settings, ...settings });
+    }
+
+    async loadSettings() {
+        this.settings = structuredClone({ ...DEFAULT_SETTINGS, ...(await this.loadData()) });
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+}
