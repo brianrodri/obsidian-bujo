a6fdd14f314ae570a7a33fea57fe9665c9b47e42
Brian Rodriguez <brian@brianrodri.com>
2023-09-24 08:40:09 -0400

    [hack] use @ungap/structured-clone to fix jsdom/jsdom#3363

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,10 @@
+import * as structuredClone from "@ungap/structured-clone";
+
 export default {
     preset: "ts-jest",
     testEnvironment: "jsdom",
     moduleDirectories: ["node_modules", "src"],
+    globals: {
+        structuredClone: structuredClone.default,
+    },
 } as import("jest").Config;
