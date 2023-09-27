99284d563538d37050021933b70a65f7fe019da4
Brian Rodriguez <brianrz@amazon.com>
2023-09-26 21:54:31 -0400

    Add ObsidianBujoIndex for tracking collections

--- src/index/__tests__/index.test.ts
+++ src/index/__tests__/index.test.ts
@@ -1,40 +1,58 @@
 import { ObsidianBujoIndex } from "index/index";
 import { describe, it, expect } from "@jest/globals";
-import { PeriodicLogConfig } from "collections/periodic-log";
+import { PeriodicLog, PeriodicLogConfig } from "collections/periodic-log";
 
-function getPeriodicLog({ ...neededByTest }: Partial<PeriodicLogConfig>) {
-    return {
-        id: "id",
-        folder: "folder",
-        fileNameFormat: "yyyy-MM-dd",
-        titleFormat: "MMMM d, yyyy",
-        period: "P1D",
-        ...neededByTest,
-    };
+const PERIODIC_ETC: PeriodicLogConfig = {
+    id: "id",
+    folder: "folder",
+    fileNameFormat: "yyyy-MM-dd",
+    titleFormat: "MMMM d, yyyy",
+    period: "P1D",
+};
+
+function newPeriodicIndex(...periodicConfigs: Partial<PeriodicLogConfig>[]) {
+    return new ObsidianBujoIndex({
+        collections: {
+            periodic: periodicConfigs.map(etc => ({ ...PERIODIC_ETC, ...etc })),
+        },
+    });
 }
+
+const withConfig = (id: string, folder: string): Partial<PeriodicLogConfig> => ({ folder, id });
 
 describe("ObsidianBujoIndex", () => {
     describe("Validation", () => {
-        it("accepts collections with unique ids", () => {
-            const validIndexProvider = () =>
-                new ObsidianBujoIndex({
-                    collections: {
-                        periodic: "a b c".split(" ").map(id => getPeriodicLog({ id })),
-                    },
-                });
-
-            expect(validIndexProvider).not.toThrow();
+        it("accepts collections with unique ids and unique folders", () => {
+            expect(() =>
+                newPeriodicIndex(
+                    withConfig("index", "/"),
+                    withConfig("hobby-ideas", "Hobbies"),
+                    withConfig("toy-collection", "Toys"),
+                ),
+            ).not.toThrow();
         });
 
-        it("rejects collections with duplicate ids", () => {
-            const validIndexProvider = () =>
-                new ObsidianBujoIndex({
-                    collections: {
-                        periodic: "a b a".split(" ").map(id => getPeriodicLog({ id })),
-                    },
-                });
+        const UNIQUE_IDS = ["index", "hobby-ideas", "toy-collection"];
+        const DUPLICATE_IDS = ["index", "toy-collection", "toy-collection"];
+        const UNIQUE_FOLDERS = ["/", "Hobbies", "Toys"];
+        const DUPLICATE_FOLDERS = ["/", "Hobbies", "Hobbies"];
 
-            expect(validIndexProvider).toThrow("identifiers must be unique");
+        it.each([
+            { ids: UNIQUE_IDS, folders: DUPLICATE_FOLDERS },
+            { ids: DUPLICATE_IDS, folders: UNIQUE_FOLDERS },
+            { ids: DUPLICATE_IDS, folders: DUPLICATE_FOLDERS },
+        ])("rejects collections when folders=%j and ids=%j (due to duplicates)", ({ ids, folders }) => {
+            expect(() => newPeriodicIndex(...ids.map((id, i) => withConfig(id, folders[i])))).toThrow("must be unique");
+        });
+    });
+
+    describe(".getCollections()", () => {
+        it("returns expected collections", () => {
+            const index = newPeriodicIndex(withConfig("/", "Index"), withConfig("Hobbies", "Ideas"));
+            expect(index.getCollections()).toEqual([
+                new PeriodicLog({ ...PERIODIC_ETC, ...withConfig("/", "Index") }),
+                new PeriodicLog({ ...PERIODIC_ETC, ...withConfig("Hobbies", "Ideas") }),
+            ]);
         });
     });
 });
--- src/index/index.ts
+++ src/index/index.ts
@@ -3,20 +3,27 @@ import { PeriodicLog } from "collections/periodic-log";
 import { ObsidianBujoSettings } from "settings/settings";
 
 export class ObsidianBujoIndex {
-    public readonly periodicLogs: PeriodicLog[];
+    public readonly periodicLogs: Map<string, PeriodicLog>;
 
     constructor(settings: ObsidianBujoSettings) {
-        this.periodicLogs = settings.collections.periodic.map(cfg => new PeriodicLog(cfg));
+        const periodicLogs = settings.collections.periodic.map(cfg => new PeriodicLog(cfg));
+        this.validateIdentifiers(...periodicLogs);
+        this.validateFolders(...periodicLogs);
 
-        this.validateIdentifiers();
+        this.periodicLogs = new Map(periodicLogs.map(log => [log.getIdentifier(), log]));
     }
 
     getCollections(): ICollection[] {
-        return [...this.periodicLogs];
+        return [...this.periodicLogs.values()] as ICollection[];
     }
 
-    private validateIdentifiers() {
-        const collections = this.getCollections();
+    private validateFolders(...logs: PeriodicLog[]) {
+        if (new Set(logs.map(l => l.folder)).size !== logs.length) {
+            throw new Error("All folders must be unique");
+        }
+    }
+
+    private validateIdentifiers(...collections: ICollection[]) {
         if (new Set(collections.map(c => c.getIdentifier())).size !== collections.length) {
             throw new Error("All identifiers must be unique");
         }
