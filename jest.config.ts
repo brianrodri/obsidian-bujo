37e2c8ff611ca67cead5e634ceb5c112a6ed3dc2
Brian Rodriguez <brian@brianrodri.com>
2023-09-28 06:07:13 -0400

    Maintain 100% coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,14 @@ import * as structuredClone from "@ungap/structured-clone";
 
 export default {
     preset: "ts-jest",
+    coverageThreshold: {
+        global: {
+            lines: 100,
+            statements: 100,
+            branches: 100,
+            functions: 100,
+        },
+    },
     collectCoverageFrom: ["src/**/*.ts", "!src/main.ts"],
     testEnvironment: "jsdom",
     moduleDirectories: ["node_modules", "src"],
